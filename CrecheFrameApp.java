83cc0afee1afdbf3103df60b76144ee1b9ca5203
Siphesihle Skosana <94805095+KayTersy06@users.noreply.github.com>
2024-08-12 12:19:21 +0200

    Add files via upload

---
+++ CrecheFrameApp.java
@@ -0,0 +1,19 @@
+package crecheframeapp;
+
+import za.ac.tut.ui.CrecheFrame;
+
+/**
+ *
+ * @author kayte
+ */
+public class CrecheFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new CrecheFrame();
+    }
+    
+}
